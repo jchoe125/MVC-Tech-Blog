5d3a126910a678266040626aa101422ac856e2cd
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-05-06 11:29:09 -0700

    adding stuff

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -16,4 +16,19 @@ router.get("/login",(req,res)=>{
     res.render("login")
 })
 
+router.get("/profile",(req,res)=>{
+    if(!req.session.user){
+        return res.redirect("/login")
+    }
+    User.findByPk(req.session.user.id,{
+        include:[Blog]
+    }).then(userData=>{
+        console.log(userData);
+        const hbsData = userData.get({plain:true})
+        console.log("=======")
+        console.log(hbsData);
+        res.render("profile",hbsData)
+    })
+})
+
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -20,3 +20,25 @@ document.querySelector("#login").addEventListener("submit",e=>{
         }
     })
 })
+
+document.querySelector("#signup").addEventListener("submit",e=>{
+    e.preventDefault();
+    const userObj = {
+        username:document.querySelector("#signupUsername").value,
+        password:document.querySelector("#signupPassword").value,
+    }
+    console.log(userObj)
+    fetch("/api/users/",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            console.log("logged in!")
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
