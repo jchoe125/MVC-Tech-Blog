080b5953a909d6ab238ed98755fa457af64b6ab5
Joe Choe <joechoe125@gmail.com>
2022-06-09 21:44:11 -0700

    minor updates

--- server.js
+++ server.js
@@ -4,18 +4,24 @@ const allRoutes = require("./controllers");
 const session = require("express-session");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require('dotenv').config();
+
+const helpers = require('./util/helper');
+
+require('dotenv').config();
 
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
-const { User, Blog } = require("./models");
+
+const { User, Blog, Comment } = require("./models");
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const sess = {
-  secret: process.env.SESSION_SECRET,
+  secret: "super super secret",
   cookie: {
     maxAge: 2 * 60 * 60 * 1000
   },
@@ -29,7 +35,7 @@ app.use(session(sess));
 // Static directory
 app.use(express.static('public'));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -40,42 +46,3 @@ sequelize.sync({ force: false }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// Sets up the Express App
-// =============================================================
-
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-// };
-
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-// };
-
-// app.use(session(sess));
-// // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // Static directory
-// app.use(express.static('public'));
-
-// const hbs = exphbs.create({});
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-// app.use('/',allRoutes);
-
-// sequelize.sync({ force: true }).then(function() {
-//     app.listen(PORT, function() {
-//     console.log('App listening on PORT ' + PORT);
-//     });
-// });
