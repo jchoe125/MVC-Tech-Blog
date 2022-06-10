1138c31f80cb2982397b6ca024a3da53885f9923
Joe Choe <joechoe125@gmail.com>
2022-06-09 22:50:44 -0700

    updated routes

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {User,Blog,Comment} = require('../models');
+const {User, Blog, Comment} = require('../models');
 const helpers = require('../util/helper')
 
 //find all blogs
