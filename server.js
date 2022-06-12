ca30894f369b90c29cf87f11ce8bbf1fcea747fc
Joe Choe <joechoe125@gmail.com>
2022-06-12 00:21:54 -0700

    updated server.js file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const { User, Blog, Comment } = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const sess = {
-  secret: process.env.DB_SESSION_SECRET,
+  secret: "super super secret",
   cookie: {
     // user session set to 30 min
     maxAge: 0.5 * 60 * 60 * 1000
